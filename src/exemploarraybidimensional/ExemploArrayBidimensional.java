49d6db51bb7ba44fa06bcd2d1282c3decc8ecd78
Ivan Steuerberg <irodriguezsteuerberg@danielcastelao.org>
2020-02-11 09:17:00 +0100

    Exemplo Matriz

---
+++ src/exemploarraybidimensional/ExemploArrayBidimensional.java
@@ -0,0 +1,36 @@
+package exemploarraybidimensional;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author irodriguezsteuerberg
+ */
+public class ExemploArrayBidimensional {
+
+    public static void main(String[] args) {
+
+int[][]matriz = new int[3][4];
+String[]alumnos={"alum1","alum2","alum3"};
+String[]modulos={"M1","M2","M3","M4"};
+int opcion;
+MetodosMatriz obx = new MetodosMatriz();
+do{
+opcion = Integer.parseInt(JOptionPane.showInputDialog("***MENU***\n1--> crear matriz"
++"\n2--> amosar \n3--> amosar todo \n4--> Sair \n Teclea a opcion"));
+switch(opcion){
+ case 1:
+    matriz = obx.crearMatriz(matriz);
+    break;
+ case 2:
+    obx.amosar(matriz);
+    break;
+ case 3:
+     obx.amosarTodo(matriz, alumnos, modulos);
+     break;
+ case 4:
+    obx.sair();
+ }
+}while(opcion<4);
+        
+}
+    
+}
